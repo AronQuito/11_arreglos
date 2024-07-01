20c11964ae83391bf5ac729c0e04e2456d5d2474
AronQuito <acachicatariq@unjbg.edu.pe>
2024-07-01 10:56:57 -0500

    Actualiza arreglo 3: Terminado

--- arreglo3.cpp
+++ arreglo3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     float TM[7];
-    int DB,TA[7]={10,2,3,4,5,9,2},TB[7]={9,1,2,3,4,8,1};
+    int p=0,k,DB,TA[7]={10,2,3,4,5,9,2},TB[7]={9,1,2,3,4,8,1};
     for (int i = 0; i<7; i++){
         TM[i] = static_cast<float>(TA[i]) / TB[i];
     }
@@ -12,8 +12,17 @@ int main(){
             DB=TB[i+1];
         }
     }
-    cin<<
-    
+    cin>>k;
+    for (int i = 0; i < 7; i++){
+        if (k==TA[i]){
+            p=1;
+            cout<<"temperatura max Coincide con el dia: "<<i+1<<endl;
+        }
+    }
+    if (p==0)
+    {
+        cout<<"El valor introducido no coincide con ninguna temperatura maxima";
+    }
     
     return 0;
 }
